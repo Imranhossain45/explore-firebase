1e1a92449b49ae6e075f749c9acf094ee77a1da3
Imranhossain45 <smimranhossain45@gmail.com>
2025-08-05 06:31:07 +0600

    Singin and out with google done

--- src/SignInWithGoogle/SignInWithGoogle.jsx
+++ src/SignInWithGoogle/SignInWithGoogle.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { app } from '../Firebase/firebaseinit';
+import React, { useState } from "react";
+import {
+  signOut,
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { app } from "../Firebase/firebaseinit";
 
 const SignInWithGoogle = () => {
+  const [user, setUser] = useState(null);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const signInBtnClick = () =>{
-    signInWithPopup(auth,provider)
-    .then(result =>{
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error =>{
-      console.log('Error' + error);
-    })
-  }
+  const signInBtnClick = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const userInfo = result.user;
+        setUser(userInfo);
+        console.log(userInfo);
+      })
+      .catch((error) => {
+        console.log("Error" + error);
+      });
+  };
+  const signOutBtn = () => {
+    signOut(auth)
+      .then((result) => {
+        setUser(null);
+        console.log('Log out done');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
-      <button onClick={signInBtnClick}>Sign In With Google</button>
+      {user && (
+        <div>
+          <img src={user.photoURL} alt="user" />
+          <h2>{user.displayName}</h2>
+          <h4>{user.email}</h4>
+        </div>
+      )}
+      {
+        user ? <button onClick={signOutBtn}>Log Out</button> :
+        <button onClick={signInBtnClick}>Sign In With Google</button>
+      
+      }
     </div>
-  )
-}
+  );
+};
 
-export default SignInWithGoogle
+export default SignInWithGoogle;
